54ccee04e3f64bc22c3504c9b1ac8fb160e6cdc7
km3ll <kmell@protonmail.com>
2024-04-12 23:35:31 -0500

    Pod 153: Synchronize pod data

---
+++ c01-intro/c01-intro.py
@@ -0,0 +1,26 @@
+
+# 2. numbers
+print("----------")
+# integers & floating-point numbers
+age = 35
+print("integer number: ", age)
+PI = 3.14159
+print("float number: ", PI)
+math_operation = 1 + 3 * 4 / 2 - 2
+print("math operation: ", math_operation)
+
+# Whenever you have a division you'll get a float type
+float_division = 8 / 3
+print("float division: 8 / 3 = ", float_division)
+
+# Integer division does not round the numbers up or down.
+# It just removes everything after the decimal place
+integer_division = 8 // 3
+print("integer division: 8 // 3 = ", integer_division)
+
+# 1. variables
+print("----------")
+user_id = 110011
+print("userId: ", user_id)
+user_id = 220022
+print("new userId: ", user_id)
--- c01-intro/main.py
+++
@@ -1,5 +0,0 @@
-# variables
-user_id = 110011
-print(user_id)
-user_id = 220022
-print(user_id)
