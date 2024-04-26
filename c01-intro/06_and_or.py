53522b4b382d10ccd1688b70bdd5d0987b6c8844
km3ll <kmell@protonmail.com>
2024-04-25 23:25:44 -0500

    Pod 153: Synchronize pod data

--- c01-intro/06_and_or.py
+++ c01-intro/06_and_or.py
@@ -16,8 +16,23 @@ print('Lists: ')
 print("bool([])", bool([]))
 print("bool([2, 4, 6])", bool([2, 4, 6]))
 
-# Falsy value with OR expression can be used for defaults
+# The `or` keyword gives us the second value if the first one is falsy.
 default_greeting = 'there'
 name = input('Enter your name: (optional) ')
 user_name = name or default_greeting
 print(f'Hello, {user_name}!')
+
+# The `and` keyword gives us the second value if the first one is truthy.
+x = True
+cmp = x and 18
+print('cmp value: ', cmp)
+
+# Keywords `and` and `or` have the same priority and evaluate left to right when in one line.
+age = 16
+side_job = True
+print(age > 18 and age <65 or side_job)
+# The conditionals evaluate first, and then `and` and `or` evaluate left to right.
+# That means that we are evaluating `False and True or True`.
+# Since we evaluate left to right, `False and True` give us `False`.
+# We are left with `False or True`, which gives us `True`.
+
