d45473e06f6b54343ed44a023ae368b94797d1c3
km3ll <kmell@protonmail.com>
2024-10-14 14:25:17 -0500

    Pod 153: Synchronize pod data

--- learn-python/s01_intro/06_numbers.py
+++ learn-python/s01_intro/06_numbers.py
@@ -6,23 +6,13 @@ PI = 3.14159
 
 # operations
 result = 1 + 3 * 4 / 2 - 2
-print("result:", math_operation)
+print("result:", result)
 
-
-# integers & floating-point numbers
-
-print("integer number: ", age)
-PI = 3.14159
-print("float number: ", PI)
-
-
-# Whenever you have a division you'll get a float type
-float_division = 8 / 3
-print("float division: 8 / 3 = ", float_division)
+# Whenever you do division you always get back a float
+float_division = 12 / 3
+print("float division: ", float_division)
 
 # Integer division does not round the numbers up or down.
 # It just removes everything after the decimal place
-integer_division = 8 // 3
-print("integer division: 8 // 3 = ", integer_division)
-
-print("> stop")
+integer_division = 12 // 3
+print("integer division:", integer_division)
